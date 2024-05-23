6078cf9097969cf08316ea83ed88abcb20ec735a
BSIT 2-1 Joshua Vidal <134849974+ItsJsh1@users.noreply.github.com>
2024-05-23 23:55:33 +0800

    Add files via upload
    Hello

---
+++ XenoClash 2.0/MainMenu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal start_game()
+
+@onready var buttons_v_box = %ButtonsVBox
+
+func _ready() -> void:
+	focus_button()
+
+func _on_start_game_pressed() -> void:
+	start_game.emit()
+	hide()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_visibility_changed():
+	if visible:
+		focus_button()
+
+func focus_button() -> void:
+	if buttons_v_box:
+		var button: Button = buttons_v_box.get_child(0)
+		if button is Button:
+			button.grab_focus()
